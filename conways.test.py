f6abe41d936db94434fa4864d578a47edce50a48
Jack Wearden <jack@jackwearden.co.uk>
2014-03-20 00:37:45 +0000

    more complex grid initialisation tests

--- conways.test.py
+++ conways.test.py
@@ -8,9 +8,18 @@ class CGOL_Tests(unittest.TestCase):
         ensure the initial grid is read correctly
 
         """
-        
+
         grid = CGOL([[True]])
         self.assertEqual(True, grid[(0,0)])
+
+        grid = CGOL([
+          [True, False, True],
+          [False, True, False],
+          [True, True, True]
+        ])
+        self.assertEqual(True, grid[(0, 0)])
+        self.assertEqual(False, grid[(0, 1)])
+        self.assertEqual(True, grid[(2, 1)])
 
 if __name__ == "__main__":
     unittest.main()
