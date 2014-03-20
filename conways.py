2b8a7752a8a6d5c589f1251d8ff390e2ba8af347
Jack Wearden <jack@jackwearden.co.uk>
2014-03-20 00:34:13 +0000

    parse initial grid

--- conways.py
+++ conways.py
@@ -3,6 +3,9 @@ from collections import defaultdict
 class CGOL:
     def __init__(self, grid):
         self._grid = defaultdict(bool)
+        for row_index, row in enumerate(grid):
+          for col_index, cell in enumerate(row):
+            self._grid[(row_index, col_index)] = cell
 
     def __next__(self):
         yield self._grid
