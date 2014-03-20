aa4abf82369368eb7324218a8dd68b9622d24b58
Jack Wearden <jack@jackwearden.co.uk>
2014-03-20 14:32:04 +0000

    consistent naming scheme

--- conways.py
+++ conways.py
@@ -3,8 +3,8 @@ from itertools import product
 class CGOL(dict):
     def __init__(self, grid):
         for row_index, row in enumerate(grid):
-          for col_index, cell in enumerate(row):
-            self[(row_index, col_index)] = cell
+          for col_index, alive in enumerate(row):
+            self[(row_index, col_index)] = alive
 
     def __iter__(self):
         return self
@@ -23,41 +23,36 @@ class CGOL(dict):
         rows = []
         for row in range(20):
             r = (self[row, col] for col in range(70))
-            rows.append("".join("+" if c else "." for c in r))
+            rows.append("".join("+" if alive else "." for alive in r))
 
         return "\n".join(rows)
 
     def __getitem__(self, key):
         return self.get(key, False)
 
-    def neighbours(self, x, y):
-        return list(set(self.cells_3x3(x, y)) - {(x, y)})
+    def neighbours(self, row, col):
+        return list(set(self.cells_3x3(row, col)) - {(row, col)})
 
-    def cells_3x3(self, x, y):
-        return list(product(range(x-1,x+2), range(y-1,y+2)))
+    def cells_3x3(self, row, col):
+        return list(product(range(row-1, row+2), range(col-1, col+2)))
 
     def cells_to_check(self):
-        cells_to_check = (self.cells_3x3(row, col) for row, col in self.keys())
-        uniques = set(elem for group in cells_to_check for elem in group)
-
-        return ((cell, self[cell]) for cell in uniques)
+        return {(cell, self[cell]) for live in self.keys()
+                                   for cell in self.cells_3x3(*live)}
 
     def next_grid(self):
       newgrid = {}
 
-      for (position, alive) in self.cells_to_check():
-          neighbour_count = sum(self[n] for n in self.neighbours(*position))
-          if alive:
-              newgrid[position] = (2 <= neighbour_count <= 3)
-          else:
-              newgrid[position] = (neighbour_count == 3)
+      for (cell, alive) in self.cells_to_check():
+          neighbour_count = sum(self[n] for n in self.neighbours(*cell))
+          newgrid[cell] = neighbour_count == 3 or alive and neighbour_count == 2
 
       return newgrid
 
     def merge(self, grid):
-        for ((row, col), cell) in grid.items():
-            self[(row, col)] = cell
-            if not cell and (row, col) in self:
+        for (cell, alive) in grid.items():
+            self[cell] = alive
+            if not alive and cell in self:
                 # we don't need to store False values explicitly since our
                 # lookup in __getitem__ uses False as a default value
-                del self[(row, col)]
+                del self[cell]
